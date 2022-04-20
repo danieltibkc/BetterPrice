3767fbccae8547e7ac71cc663a93348aa8b4b1d7
Daniel Tibaquira <esteban.tibaquira@kinandcarta.com>
2022-04-20 12:23:09 -0500

    Setting up testing

---
+++ __tests__/App.test.js
@@ -0,0 +1,12 @@
+import renderer from "react-test-renderer";
+import App from "../App.js";
+
+describe("<App />", () => {
+
+  it("has one child", () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    expect(tree.children.length).toBe(1);
+  });
+
+});
