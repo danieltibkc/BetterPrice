3576ef8d90630985003870516b1794df372e8abd
Daniel Tibaquira <esteban.tibaquira@kinandcarta.com>
2022-04-20 13:51:48 -0500

    Setting up CI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # BetterPrice
+
 A KAP Project to showcase skills learned during training.
 
 ### What is it?
+
 A small app for AirBnB renters to identify how they can get better prices for their places.
--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,11 +2,9 @@ import renderer from "react-test-renderer";
 import App from "../App.js";
 
 describe("<App />", () => {
+	it("has one child", () => {
+		const tree = renderer.create(<App />).toJSON();
 
-  it("has one child", () => {
-    const tree = renderer.create(<App />).toJSON();
-
-    expect(tree.children.length).toBe(1);
-  });
-
+		expect(tree.children.length).toBe(1);
+	});
 });
