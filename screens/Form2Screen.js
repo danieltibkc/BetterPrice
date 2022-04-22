5a0b4962c5701de8a4e32c5f816aee9efd58e5cc
Daniel Tibaquira <esteban.tibaquira@kinandcarta.com>
2022-04-22 17:23:08 -0500

    Feature/US-Navigation: Changing header navigation

--- screens/Form2Screen.js
+++ screens/Form2Screen.js
@@ -1,18 +1,36 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
+import { StyleSheet, Text, View } from "react-native";
 
-const Form2Screen = ({navigation}) => {
+const Form2Screen = ({ navigation }) => {
+
+	const handleGoToForm1 = () => {
+		navigation.goBack();
+	};
 
 	const handleGoToResults = () => {
-	  navigation.navigate("Results");
+		navigation.navigate("Results");
 	};
 
 	return (
 		<View style={styles.rootContainer}>
-			<Text>Form1Screen</Text>
-			<Pressable onPress={handleGoToResults}>
-				<Text style={styles.buttonText}>Go to Results Screen</Text>
-			</Pressable>
+			<View style={styles.navigationControlStyle}>
+				<Ionicons
+					name="arrow-back"
+					size={36}
+					color={"#007bff"}
+					onPress={handleGoToForm1}
+				/>
+				<Ionicons
+					name="arrow-forward"
+					size={36}
+					color={"#007bff"}
+					onPress={handleGoToResults}
+				/>
+			</View>
+			<View style={styles.screenNameStyle}>
+				<Text>Form1Screen</Text>
+			</View>
 		</View>
 	);
 };
@@ -20,11 +38,21 @@ const Form2Screen = ({navigation}) => {
 Form2Screen.propTypes = {
 	navigation: PropTypes.shape({
 		navigate: PropTypes.func.isRequired,
+		goBack: PropTypes.func.isRequired
 	}).isRequired,
 };
 
 const styles = StyleSheet.create({
 	rootContainer: {
+		flex: 1
+	},
+	navigationControlStyle: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingHorizontal: 18,
+		paddingVertical: 36
+	},
+	screenNameStyle: {
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center"
