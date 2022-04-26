c4f0f83868d1cd9cafca8210529ec3a34907da59
Daniel Tibaquira <esteban.tibaquira@kinandcarta.com>
2022-04-26 16:19:50 -0500

    Feature/US-Navigation: Fixed navigation testing where "goBack" behavior wasn't working

--- __tests__/Navigation.test.js
+++ __tests__/Navigation.test.js
@@ -1,37 +1,89 @@
-import { fireEvent, render } from "@testing-library/react-native";
-import Form1Screen from "../screens/Form1Screen";
-import Form2Screen from "../screens/Form2Screen";
+import { cleanup, fireEvent, render } from "@testing-library/react-native";
+import App from "../App";
 
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+afterEach(cleanup);
 
 describe("Testing React Navigation", () => {
-	test("Home Screen navigates to Search Location Screen", () => {
-		const navigate = jest.fn();
+  //	App Navigation Structure:
+  //		Stack:
+  //			Stack:
+  //				Form1Screen
+  //				SearchLocationScreen
+  //			Form2Screen
+  //			ResultsScreen
 
-		const { getByText } = render(<Form1Screen navigation={{ navigate }} />);
+  test("Home Screen navigates to Search Location Screen", () => {
+    const { getByText } = render(<App />);
+    // Form1Screen go to SearchLocationScreen
+    fireEvent.press(getByText("Go to Search Location Screen"));
 
-		fireEvent.press(getByText("Go to Search Location Screen"));
-		expect(navigate).toHaveBeenCalledWith("SearchLocation");
-	});
+    const searchLocationText = getByText("SearchLocationScreen");
+    expect(searchLocationText).toBeTruthy();
+  });
 
-	test("Home Screen navigates to Form 2 Screen", () => {
-		const navigate = jest.fn();
+  test("Home Screen navigates to Form 2 Screen", () => {
+    const { getByText, getByA11yHint } = render(<App />);
+    // Form1Screen go to Form2Screen
+    fireEvent.press(getByA11yHint("goToForm2Button"));
 
-		const { getByA11yHint } = render(<Form1Screen navigation={{ navigate }} />);
+    const searchLocationText = getByText("Form2Screen");
+    expect(searchLocationText).toBeTruthy();
+  });
 
-		fireEvent.press(getByA11yHint("goToForm2Button"));
-		expect(navigate).toHaveBeenCalledWith("Form2");
-	});
+  test("Form 2 Screen navigates to Results Screen", () => {
+    const { getByText, getByA11yHint } = render(<App />);
+    // Form1Screen go to Form2Screen
+    fireEvent.press(getByA11yHint("goToForm2Button"));
 
-	test("Form 2 Screen navigates to Results Screen", () => {
-		const navigate = jest.fn();
-		const goBack = jest.fn();
+    // Form2Screen go to ResultsScreen
+    fireEvent.press(getByA11yHint("goToResultsButton"));
 
-		const { getByA11yHint } = render(
-			<Form2Screen navigation={{ navigate, goBack }} />
-		);
+    const resultsText = getByText("ResultsScreen");
+    expect(resultsText).toBeTruthy();
+  });
 
-		fireEvent.press(getByA11yHint("goToResultsButton"));
-		expect(navigate).toHaveBeenCalledWith("Results");
-	});
+  test("Form 2 Screen navigates Back", () => {
+    const { getByText, getByA11yHint } = render(<App />);
+    // Form1Screen go to Form2Screen
+    fireEvent.press(getByA11yHint("goToForm2Button"));
+
+    // Form2Screen go Back
+    fireEvent.press(getByA11yHint("goBackButton"));
+    const form1Text = getByText("Form1Screen");
+    const searchText = getByText("Go to Search Location Screen");
+
+    expect(form1Text).toBeTruthy();
+    expect(searchText).toBeTruthy();
+  });
+
+  test("Search Location Screen navigates Back", () => {
+    const { getByText, getByA11yHint } = render(<App />);
+    // Form1Screen go to SearchLocationScreen
+    fireEvent.press(getByText("Go to Search Location Screen"));
+
+    // SearchLocationScreen go Back
+    fireEvent.press(getByA11yHint("goBackButton"));
+    const form1Text = getByText("Form1Screen");
+    const searchText = getByText("Go to Search Location Screen");
+
+    expect(form1Text).toBeTruthy();
+    expect(searchText).toBeTruthy();
+  });
+
+  test("Results Screen navigates Back", () => {
+    const { getByText, getByA11yHint } = render(<App />);
+    // Form1Screen go to Form2Screen
+    fireEvent.press(getByA11yHint("goToForm2Button"));
+
+    // Form2Screen go to ResultsScreen
+    fireEvent.press(getByA11yHint("goToResultsButton"));
+
+    // Form2Screen go to ResultsScreen
+    fireEvent.press(getByA11yHint("goBackButton"));
+
+    const form2Text = getByText("Form2Screen");
+
+    expect(form2Text).toBeTruthy();
+  });
 });
