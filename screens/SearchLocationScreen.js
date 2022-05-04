29df1aa003fda6d9c3bc26bd3d14fb731484744d
Daniel Tibaquira <esteban.tibaquira@kinandcarta.com>
2022-05-04 10:15:47 -0500

    Feature/US-Maps: Now using Google Maps on both iOS and Android, Added Custom Map Marker and Integrated Geocode API

--- screens/SearchLocationScreen.js
+++ screens/SearchLocationScreen.js
@@ -3,89 +3,110 @@ import { StyleSheet, View } from "react-native";
 import NavigateButton from "../components/UI/NavigateButton";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { geocodeAPI } from "../util/http";
 
 const SearchLocationScreen = ({ navigation }) => {
-  const handleGoToForm = () => {
-    navigation.goBack();
-  };
+	const handleGoToForm = () => {
+		navigation.goBack();
+	};
 
-  const [selectedLocation, setSelectedLocation] = useState(null);
-  const [location, setLocation] = useState(null);
-  // const [errorMsg, setErrorMsg] = useState(null);
+	const [selectedLocation, setSelectedLocation] = useState(null);
+	const [locationInfo, setLocationInfo] = useState("");
+	const [location, setLocation] = useState({
+		latitude: 37.78,
+		longitude: -122.43,
+	});
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        // setErrorMsg("Permission to access location was denied");
-        return;
-      }
+	// const [errorMsg, setErrorMsg] = useState(null);
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				// setErrorMsg("Permission to access location was denied");
+				return;
+			}
 
-  const initialRegion = {
-    latitude: location ? location.latitude : 37.78,
-    longitude: location ? location.longitude : -122.43,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  };
+			let currLocation = await Location.getCurrentPositionAsync({});
+			setLocation({
+				latitude: currLocation.latitdude,
+				longitude: currLocation.longitude,
+			});
+		})();
+	}, []);
 
-  function selectLocationHandler(event) {
-    const lat = event.nativeEvent.coordinate.latitude;
-    const lng = event.nativeEvent.coordinate.longitude;
+	const initialRegion = {
+		latitude: 37.78825,
+		longitude: -122.4324,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421,
+	};
 
-    setSelectedLocation({ lat: lat, lng: lng });
-  }
+	const selectLocationHandler = async (event) => {
+		const lat = event.nativeEvent.coordinate.latitude;
+		const lng = event.nativeEvent.coordinate.longitude;
+		location;
+		setSelectedLocation({ lat: lat, lng: lng });
+		await getPlaceInfo(lat, lng);
+	};
 
-  return (
-    <View style={styles.rootContainer} accessibilityHint="SearchLocationScreen">
-      <NavigateButton
-        onPress={handleGoToForm}
-        accessibilityHint="goBackButton"
-        direction="back"
-      />
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={initialRegion}
-        onPress={selectLocationHandler}
-      >
-        {selectedLocation && (
-          <Marker
-            title="Picked Location"
-            coordinate={{
-              latitude: selectedLocation.lat,
-              longitude: selectedLocation.lng,
-            }}
-          />
-        )}
-      </MapView>
-    </View>
-  );
+	const getPlaceInfo = async (lat, lng) => {
+		const places = await geocodeAPI(lat, lng);
+		const placeName = places.data.data[0].label;
+		setLocationInfo(placeName);
+	};
+
+	return (
+		<View style={styles.rootContainer} accessibilityHint="SearchLocationScreen">
+			<NavigateButton
+				onPress={handleGoToForm}
+				accessibilityHint="goBackButton"
+				direction="back"
+			/>
+			<MapView
+				provider={PROVIDER_GOOGLE}
+				showsUserLocation
+				showsMyLocationButton
+				zoomEnabled
+				style={{ flex: 1 }}
+				initialRegion={initialRegion}
+				onPress={selectLocationHandler}
+			>
+				{selectedLocation && (
+					<Marker
+						title={locationInfo}
+						image={require("../assets/marker.png")}
+						coordinate={{
+							latitude: selectedLocation.lat,
+							longitude: selectedLocation.lng,
+						}}
+					/>
+				)}
+			</MapView>
+		</View>
+	);
 };
 
 SearchLocationScreen.propTypes = {
-  navigation: PropTypes.shape({
-    goBack: PropTypes.func.isRequired,
-  }).isRequired,
+	navigation: PropTypes.shape({
+		goBack: PropTypes.func.isRequired,
+	}).isRequired,
 };
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-  },
-  linkContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "#007bff",
-    margin: 8,
-  },
+	rootContainer: {
+		flex: 1,
+	},
+	linkContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	buttonText: {
+		color: "#007bff",
+		margin: 8,
+	},
 });
 
 export default SearchLocationScreen;
--- util/http.js
+++ util/http.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const baseURL = "http://127.0.0.1:8000/";
 const predictionAPIURL = "api/predictions/";
+const GEOCODE_API_KEY = "275d91f9b401123f43c95abbc960630e";
+const geocodeBaseURL = "http://api.positionstack.com/v1/";
+const geocodeReverse = `${geocodeBaseURL}reverse?access_key=${GEOCODE_API_KEY}&query=`;
 
 export const predictAPI = async (dispatch, getState) => {
 	const placeFeatures = getState().features;
@@ -10,4 +13,11 @@ export const predictAPI = async (dispatch, getState) => {
 	const parsedResponse = await JSON.parse(res.data);
 
 	dispatch({ type: "LOAD", payload: { ...parsedResponse } });
+};
+
+export const geocodeAPI = async (lat, long) => {
+	const queryURL = `${geocodeReverse}${lat},${long}`;
+	const response = await axios.get(queryURL);
+
+	return response;
 };
