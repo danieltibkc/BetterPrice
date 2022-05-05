cc0b37c758426dd13ac1fff74f6c51afe6c82fe9
Daniel Tibaquira <esteban.tibaquira@kinandcarta.com>
2022-05-05 09:55:13 -0500

    Feature/US-Maps: Added Progressive Loader Views waiting for API response, modified mapStyle

--- screens/SearchLocationScreen.js
+++ screens/SearchLocationScreen.js
@@ -7,147 +7,178 @@ import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { geocodeAPI } from "../util/http";
 import Card from "../components/UI/Card";
 import { Globals } from "../constants/styles";
+import { MapStyle } from "../util/mapStyle";
 
 const SearchLocationScreen = ({ navigation }) => {
-	const handleGoToForm = () => {
-		navigation.goBack();
-	};
+  const handleGoToForm = () => {
+    navigation.goBack();
+  };
 
-	const [errorMsg, setErrorMsg] = useState(null);
-	const [selectedLocation, setSelectedLocation] = useState(null);
-	const [locationInfo, setLocationInfo] = useState({});
-	const [location, setLocation] = useState({
-		latitude: 37.78,
-		longitude: -122.43,
-	});
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [locationInfo, setLocationInfo] = useState("");
+  const [location, setLocation] = useState({
+    latitude: 37.78,
+    longitude: -122.43,
+  });
 
-	useEffect(() => {
-		(async () => {
-			let { status } = await Location.requestForegroundPermissionsAsync();
-			if (status !== "granted") {
-				setErrorMsg("Permission to access location was denied");
-				return;
-			}
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
 
-			let currLocation = await Location.getCurrentPositionAsync({});
-			setLocation({
-				latitude: currLocation.latitdude,
-				longitude: currLocation.longitude,
-			});
-			location;
-		})();
-	}, []);
+      let currLocation = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: currLocation.latitdude,
+        longitude: currLocation.longitude,
+      });
+      location;
+    })();
+  }, []);
 
-	const initialRegion = {
-		latitude: 37.78825,
-		longitude: -122.4324,
-		latitudeDelta: 0.0922,
-		longitudeDelta: 0.0421,
-	};
+  const initialRegion = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
-	const selectLocationHandler = async (event) => {
-		const lat = event.nativeEvent.coordinate.latitude;
-		const lng = event.nativeEvent.coordinate.longitude;
-		setSelectedLocation({ lat: lat, lng: lng });
-		await getPlaceInfo(lat, lng);
-	};
+  const selectLocationHandler = async (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat: lat, lng: lng });
+    await getPlaceInfo(lat, lng);
+  };
 
-	const getPlaceInfo = async (lat, lng) => {
-		try {
-			const places = await geocodeAPI(lat, lng);
-			const placeName = places.data.data[0];
-			setLocationInfo(placeName);
-		} catch (e) {
-			setErrorMsg(e);
-		}
-	};
+  const getPlaceInfo = async (lat, lng) => {
+    try {
+      const places = await geocodeAPI(lat, lng);
+      const placeName = places.data.data[0];
+      // setTimeout(() => setLocationInfo(placeName), 2000);
+      setLocationInfo(placeName);
+    } catch (e) {
+      setErrorMsg(e);
+    }
+  };
 
-	return (
-		<View style={styles.rootContainer} accessibilityHint="SearchLocationScreen">
-			<NavigateButton
-				onPress={handleGoToForm}
-				accessibilityHint="goBackButton"
-				direction="back"
-			/>
-			<MapView
-				provider={PROVIDER_GOOGLE}
-				showsUserLocation
-				showsMyLocationButton
-				zoomEnabled
-				style={{ flex: 1 }}
-				initialRegion={initialRegion}
-				onPress={selectLocationHandler}
-			>
-				{selectedLocation && (
-					<Marker
-						image={require("../assets/marker.png")}
-						coordinate={{
-							latitude: selectedLocation.lat,
-							longitude: selectedLocation.lng,
-						}}
-					>
-						<Callout tooltip>
-							<Card style={{ width: 150, alignItems: "center" }}>
-								<Text
-									style={{
-										fontSize: 24,
-										color: Globals.colors.text,
-										fontWeight: "bold",
-										textAlign: "center",
-									}}
-								>
-                  Address:
-								</Text>
-								<Text
-									style={{
-										fontWeight: "300",
-										fontSize: 18,
-										textAlign: "center",
-									}}
-								>
-									{locationInfo.name}
-								</Text>
-							</Card>
-						</Callout>
-					</Marker>
-				)}
-				{errorMsg && (
-					<View
-						style={{
-							alignSelf: "flex-end",
-							height: 60,
-							backgroundColor: "red",
-							margin: 30,
-							padding: 20,
-						}}
-					>
-						<Text style={{ color: "white" }}>{errorMsg}</Text>
-					</View>
-				)}
-			</MapView>
-		</View>
-	);
+  const PlaceInfoCallout = () => {
+    return (
+      <>
+        <Text
+          style={{
+            fontSize: 24,
+            color: Globals.colors.text,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Address:
+        </Text>
+        <Text
+          style={{
+            fontWeight: "300",
+            fontSize: 18,
+            textAlign: "center",
+          }}
+        >
+          {locationInfo.name}
+        </Text>
+      </>
+    );
+  };
+
+  const PlaceInfoCalloutPlaceholder = () => {
+    return (
+      <View style={{ alignSelf: "center" }}>
+        <View
+          style={[styles.locationInfoPlaceholder, { width: 50, margin: 8 }]}
+        />
+        <View style={styles.locationInfoPlaceholder} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.rootContainer} accessibilityHint="SearchLocationScreen">
+      <NavigateButton
+        onPress={handleGoToForm}
+        accessibilityHint="goBackButton"
+        direction="back"
+      />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+        zoomEnabled
+        customMapStyle={MapStyle}
+        style={{ flex: 1 }}
+        initialRegion={initialRegion}
+        onPress={selectLocationHandler}
+      >
+        {selectedLocation && (
+          <Marker
+            image={require("../assets/marker.png")}
+            coordinate={{
+              latitude: selectedLocation.lat,
+              longitude: selectedLocation.lng,
+            }}
+          >
+            <Callout tooltip>
+              <Card style={{ minWidth: 150, maxWidth: 250, padding: 8 }}>
+                {locationInfo !== "" ? (
+                  <PlaceInfoCallout />
+                ) : (
+                  <PlaceInfoCalloutPlaceholder />
+                )}
+              </Card>
+            </Callout>
+          </Marker>
+        )}
+        {errorMsg && (
+          <View
+            style={{
+              alignSelf: "flex-end",
+              height: 60,
+              backgroundColor: "red",
+              margin: 30,
+              padding: 20,
+            }}
+          >
+            <Text style={{ color: "white" }}>{errorMsg}</Text>
+          </View>
+        )}
+      </MapView>
+    </View>
+  );
 };
 
 SearchLocationScreen.propTypes = {
-	navigation: PropTypes.shape({
-		goBack: PropTypes.func.isRequired,
-	}).isRequired,
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 const styles = StyleSheet.create({
-	rootContainer: {
-		flex: 1,
-	},
-	linkContainer: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	buttonText: {
-		color: "#007bff",
-		margin: 8,
-	},
+  rootContainer: {
+    flex: 1,
+  },
+  linkContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#007bff",
+    margin: 8,
+  },
+  locationInfoPlaceholder: {
+    backgroundColor: "#e0e0e0",
+    flex: 1,
+    padding: 8,
+  },
 });
 
 export default SearchLocationScreen;
